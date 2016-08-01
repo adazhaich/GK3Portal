9e312ae4bf69b0e7099cf915a62ca54d7fc56cb7
avandana <ashok.vandana@mobiusws.com>
2016-08-01 15:49:10 -0400

    1.Context root reference for SIGN-UP page

---
+++ views/js/signup.js
@@ -0,0 +1,12 @@
+/**
+ * Created by avandana on 7/21/2016.
+ */
+/**
+ * Created by avandana on 7/21/2016.
+ */
+$(document).ready(function(){
+    //console.log("Inside login.js ");
+
+    $("#loginUser").attr("action", "/test/login");
+    $("#homeLink").attr("href", "/test");
+});
