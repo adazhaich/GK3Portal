0983ddae12969bc7edea4750e3c1b9c18c1a91e9
avandana <ashok.vandana@mobiusws.com>
2016-08-09 11:13:30 -0400

    1Use Connection pool for user login and creating new users

--- app/passport.js
+++ app/passport.js
@@ -1,40 +1,14 @@
-
 var LocalStrategy = require('passport-local').Strategy;
-var logger = require('./logger.js');
-var mysql = require('mysql');
+var logger = require("./logger.js");
 var bcrypt = require('bcrypt-nodejs');
-
-// load the auth variables
-//var configAuth = require('./auth'); // use this one for testing
-
-/* ===============USE THIS TO TEST DB CONNECTION DURING SERVER START UP FOR NEW DEPLOYMENTS==================================
- connection.connect(function(err){
- if(!err) {
- //console.log("Database is connected ... \n\n");
- //logger.debug("Using Database:", gk3_accounts_db.database);
- connection.query('USE ' + gk3_accounts_db.database);
- //console.log("Login database:", gk3_accounts_db.database);
- logger.debug("SUCCESS Connecting to GK3 Accounts Login Database", gk3_accounts_db.database);
-
- } else {
- logger.debug("ERROR connecting to GK3 Accounts Login database: \n\n",gk3_accounts_db.database);
- }
- });
- ===============USE THIS TO TEST DB CONNECTION DURING SERVER START UP FOR NEW DEPLOYMENTS================================== */
+var config = require("./config");
 
 
 module.exports = function (passport, gk3_accounts_pool, logger) {
-
     passport.serializeUser(function (user, finished) {
         logger.debug("passport.serializeUser:user:", user);
         finished(null, user);  // done(null, user.id); Saving entire user object into session
     });
-
-    /*	In deserialize function you provide in first argument of deserialize function that same key of user object that was given to done function in
-     serialize call. So your whole object is retrieved with help of that key. That key here is id(key can be any key of the user object ie name,email etc).
-     In deserialize function that key is matched with in memory array / database or any data resource.
-     The fetched object is attached to request object as req.user*/
-    //passport.deserializeUser(function(req,id,done) {
 
     passport.deserializeUser(function (user, done) {
         if (user) {
@@ -42,12 +16,10 @@ module.exports = function (passport, gk3_accounts_pool, logger) {
         }
         else {
             logger.debug("request is NOT coming from same user");
-            err = "INVALID USER REQUEST.PLEASE LOGIN or CHECK IF YOU HAVE ACCESS"
+            err = "INVALID USER REQUEST.PLEASE LOGIN or CHECK IF YOU HAVE ACCESS";
             done(err);
         }
-
     });
-
 
     // =========================================================================
     // LOCAL LOGIN =============================================================
@@ -61,41 +33,27 @@ module.exports = function (passport, gk3_accounts_pool, logger) {
         function (req, username, password, done) {
             if (username)
                 username = username.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
-
             logger.debug("***INSIDE passport local-login ******\n\n");
-            gk3_accounts_pool.getConnection(function (err, connection) {
+            gk3_accounts_pool.query("select a.id,a.username,a.password,group_concat(role_id) role_id from (select * from users where username=?) " +
+                "a left join (select * from user_roles where DELETED_BY_USER_ID is null) b on a.id=b.user_id group by a.id", [username], function (err, rows) {
 
                 if (err) {
-                    connection.release();
-                    //res.json({"code" : 100, "status" : "Error in connection database"});
-                    logger.debug("***local-login**** Error connecting database ... \n\n", err);
-                    return;
+                    logger.debug("***local-login**** Error running login SQL ... ", err);
+                    //logger.debug("***local-login**** Error for USER ... ", username);
+                    //logger.debug("***local-login**** Error for USER pwd ...", password);
+
+                    return done(err);
                 }
-                // asynchronous
-                connection.query("select a.id,a.username,a.password,group_concat(role_id) role_id from (select * from users where username=?) a left join (select * from user_roles where DELETED_BY_USER_ID is null) b on a.id=b.user_id group by a.id", [username], function (err, rows) {
-                    connection.release();
-                    if (err) {
-
-                        logger.debug("***local-login**** Error running login SQL ... ", err);
-                        //logger.debug("***local-login**** Error for USER ... ", username);
-                        //logger.debug("***local-login**** Error for USER pwd ...", password);
-
-                        return done(err);
-                    }
-                    if (!rows.length) {
-                        //connection.release();
-                        logger.debug("***local-login**** NO USER FOUND...***");
-                        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                    }
-                    if (!bcrypt.compareSync(password, rows[0].password)) {
-                        //connection.release();
-                        logger.debug("***local-login**** WRONG PASSWORD...***");
-                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-                    }
-                    //connection.release();
-                    logger.debug("passport.login-login SUCCESS for user:", rows[0]);
-                    return done(null, rows[0]); //LOGIN SUCCESS-calls passport.serialize to serialize user into session
-                });
+                if (!rows.length) {
+                    logger.debug("***local-login**** NO USER FOUND...***");
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                }
+                if (!bcrypt.compareSync(password, rows[0].password)) {
+                    logger.debug("***local-login**** WRONG PASSWORD...***");
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                }
+                logger.debug("passport.login-login SUCCESS for user:", rows[0]);
+                return done(null, rows[0]); //LOGIN SUCCESS-calls passport.serialize to serialize user into session
             });
         }//end of function
     ));
@@ -114,53 +72,40 @@ module.exports = function (passport, gk3_accounts_pool, logger) {
             if (username)
                 username = username.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
 
-            gk3_accounts_pool.getConnection(function (err, connection) {
+            gk3_accounts_pool.query("SELECT * FROM users WHERE username = ?", [username], function (err, rows) {
+
                 if (err) {
-                    //console.log("Error connecting database ... \n\n", err);
-                    //logger.debug("Error connecting database ... \n\n", err);
-                    connection.release();
-                    return;
+                    return done(err);
                 }
+                if (rows.length) {
+                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                } else {
+                    // if there is no user with that usernam, create the NEW user
+                    var newUserMysql = {
+                        username: username,
+                        password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
+                    };
 
-                // asynchronous
-                connection.query("SELECT * FROM users WHERE username = ?", [username], function (err, rows) {
+                    var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
 
-                    if (err) {
-                        connection.release();
-                        return done(err);
-                    }
-                    if (rows.length) {
-                        connection.release();
-                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                    } else {
-                        // if there is no user with that usernam, create the NEW user
-                        var newUserMysql = {
-                            username: username,
-                            password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
-                        };
+                    gk3_accounts_pool.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
+                        if (err) {
+                            return done(err);
+                        }
+                        else {
+                            newUserMysql.id = rows.insertId;
+                            return done(null, newUserMysql);
+                        }
+                    });
+                }
+            });//finished asynchronous
 
-                        var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
-
-                        connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
-                            connection.release();
-                            if (err) {
-                                return done(err);
-                            }
-                            else {
-                                newUserMysql.id = rows.insertId;
-                                return done(null, newUserMysql);
-                            }
-                        });
-                    }
-                });//finished asynchronous
-            });
         }//end function
     ));
 };
 
 
-/*
-
+/*=======================================TO READ ============================================================================================================================
 
  First you must understand what serialize and deserialize are meant for.
 
@@ -171,12 +116,7 @@ module.exports = function (passport, gk3_accounts_pool, logger) {
  where else done(err,null) removes it from the session, redirecting you to whatever your app.get('/auth/:provider/callback')
  sends the user to after checking if the session is timed out or not. This should clarify things for your second question.*/
 
-
-
-//var uname;
-
 //https://github.com/jaredhanson/passport
-
 
 /*
  // =========================================================================
@@ -205,3 +145,10 @@ module.exports = function (passport, gk3_accounts_pool, logger) {
  The key of user object you provide in second argument of the done in serialize function is saved in session and is used to retrieve the whole object via deserialize function.
  Serialize function determine what data from the user object should be stored in the session. The result of the serializeUser method is attached to the session as req.session.passport.user = {}
  here for instance it would be(as we provide id as key) req.session.passport.user = {id:'xyz'}*/
+
+/*	In deserialize function you provide in first argument of deserialize function that same key of user object that was given to done function in
+ serialize call. So your whole object is retrieved with help of that key. That key here is id(key can be any key of the user object ie name,email etc).
+ In deserialize function that key is matched with in memory array / database or any data resource.
+ The fetched object is attached to request object as req.user
+
+ =======================================================================================================================================================================================*/
