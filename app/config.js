9bb5c0b1cc2c5b470f06fdeedfae85f0b7298ee5
Aether <aether.ji@mobiusws.com>
2016-08-30 10:35:44 +0800

    fix search input size

--- app/config.js
+++ app/config.js
@@ -1,26 +1,27 @@
-//NOTE: Change SSL to true and comment out the port number while deploying in DEMO or PROD servers
 module.exports = {
     "httpConfig": {
         "contextRoot": "/tunisiana",
         "default_illegal_odds": 0.7,
         "checkAuthentication": true,
         "host": "localhost",
-        "ssl": true,
+        "ssl": false,
         "port": 8580,
         "sslPort": 8443
     },
     "mysqlConfig": {
-        "host": "10.212.2.173", //NEW SERVER
+        "host": "127.0.0.1", //NEW SERVER
         "port": "3306",  //NEW PORT
-        "user": "mwsadmin",
-        "password": "ubi6La5z",
+       // "user": "mwsadmin",
+        //"password": "ubi6La5z",
+        "user":"root",
+        "password":"great",
         "debug": false,
         "database": "gk3_accounts"
     },
     "dataService": {
-        "url": "10.212.2.143", //phoenix service
+        "url": "127.0.0.1", //phoenix service
         "protocol": "http",
-        "port": 8080
+        "port": 8900
     },
     "defaultTimeConfig": {
         "range_hour": "5"
@@ -32,4 +33,3 @@ module.exports = {
         "logsDirectory": "__dirname + /logs/"
     }
 };
-
